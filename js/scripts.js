40017f6bf70235106689bdc034a476e6ba610235
Rebecca McIntosh and senya webster <senya@>
2017-08-07 13:19:37 -0700

    add BMI calculator and farenheit to celsius calculator

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,60 @@ var number1= parseInt(prompt("enter a number"));
 var number2=parseInt(prompt("enter another number"));
 
 alert(add(number1, number2));
+
+
+
+var subtract = function(number1, number2){
+  return number1 - number2;
+};
+
+var number1= parseInt(prompt("enter a number"));
+var number2=parseInt(prompt("enter another number"));
+
+alert(subtract(number1, number2));
+
+
+
+var multiply = function(number1, number2){
+  return number1 * number2;
+};
+
+var number1= parseInt(prompt("enter a number"));
+var number2=parseInt(prompt("enter another number"));
+
+alert(multiply(number1, number2));
+
+
+
+var divide = function(number1, number2){
+  return number1 / number2;
+};
+
+var number1= parseInt(prompt("enter a number"));
+var number2=parseInt(prompt("enter another number"));
+
+alert(divide(number1, number2));
+
+
+
+// <<------BMI Calculator-------->>
+var BMI = function (height, weight){
+	return weight / Math.sqrt(height);
+}
+
+var height = parseInt(prompt("enter height"));
+var weight = parseInt(prompt("enter weight"));
+
+alert(BMI(height, weight))
+
+
+// <<------Farenheit to Celsius-------->>
+var celsius = function(farenheit, multiply, add) {
+	return farenheit * multiply + add;
+}
+
+var farenheit = parseInt(prompt("enter farenheit"));
+var multiply = 1.8;
+var add = 32;
+
+alert(celsius(farenheit, multiply, add));
